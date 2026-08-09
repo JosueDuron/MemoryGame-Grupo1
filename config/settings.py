60245cab81783e985fe2d6d7eb76e2df97ea047c
JosueDuron <duronjosue189@gmail.com>
2026-08-08 22:06:13 -0600

    Proyecto

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,13 +82,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.environ.get(
-            'SQLITE_PATH',
-            BASE_DIR / 'db.sqlite3'
-        ),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('postgresql://postgres:vqxcREaVnTBEPvPSQjsLxpswUFmSYBDC@postgres.railway.internal:5432/railway'),
+        conn_max_age=600
+    )
 }
 
 
